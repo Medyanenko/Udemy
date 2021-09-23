27b709dfedf8124b6f932128706b32c3110c7a4c
Tatiana <t.kudryavska@gmail.com>
2021-09-23 12:07:44 +0300

    First Task - numberOfFilms

--- test.js
+++ test.js
@@ -1,12 +1,19 @@
-"use sctrict";
+"use strict";
 
-
-let number = 5;
-const leftBorderWidth = 1;
-
-console.log(number);
-const obj = {
-    a: 50
+const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели", "");
+const personalMovieDB ={
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
-obj.a  = 35;
-console.log (obj.a);
+const a = prompt("Один из последних просмотренных фильмов", ""),
+      b = prompt("На сколько оцените его?", ""),
+      c = prompt("Один из последних просмотренных фильмов", ""),
+      d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b; 
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
